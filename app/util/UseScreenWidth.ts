2a0c9d0811fa1aa8ca7f76add1ddef4345488718
lts-pj <lliamsymonds04@gmail.com>
2025-01-29 09:43:58 +1000

    moved accessing window to the hook

--- app/util/UseScreenWidth.ts
+++ app/util/UseScreenWidth.ts
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 export default function useScreenWidth() {
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const [screenWidth, setScreenWidth] = useState(1920);
 
   useEffect(() => {
     const handleResize = () => setScreenWidth(window.innerWidth);
